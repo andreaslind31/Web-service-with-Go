7fca7b52ea24a06e8f36179fa092058b276e485c
Andreas Lind <Andreaslind31@gmail.com>
2021-06-14 20:51:52 +0200

    productHandler added

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Product struct {
@@ -63,6 +65,14 @@ func getNextId() int {
 	}
 	return highestId +1
 }
+func findProductById(productId int) (*Product, int)  {
+	for i, product := range productList {
+		if product.ProductID == productId {
+			return &product, i
+		}
+	}
+	return nil, 0
+}
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -96,7 +106,55 @@ func productsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+func productHandler(w http.ResponseWriter, r *http.Request)  {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+	productId, err := strconv.Atoi((urlPathSegments[len(urlPathSegments)-1]))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	product, listItemIndex := findProductById(productId)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	switch r.Method{
+	case http.MethodGet:
+		productJSON, err := json.Marshal(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJSON)
+	case http.MethodPut:
+		//update product in the list
+		var updatedProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &updatedProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updatedProduct.ProductID != productId {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		product = &updatedProduct 
+		productList[listItemIndex] = *product
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	
+}
 func main() {
 	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 	http.ListenAndServe(":5000", nil)
 }
